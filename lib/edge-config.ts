a7d1595b7a610f0f81560fbb04a98d1c6bb3d87a
mnerpyctno <artsemi.shyshkin@gmail.com>
2025-03-30 21:47:40 +0300

    Исправлен URL для Upstash Redis REST API

--- lib/edge-config.ts
+++ lib/edge-config.ts
@@ -2,7 +2,7 @@ import { Redis } from '@upstash/redis';
 import { Store, MonthlyPlan } from '@/app/types';
 
 const redis = new Redis({
-  url: process.env.REDIS_URL || '',
+  url: process.env.KV_REST_API_URL || '',
   token: process.env.KV_REST_API_TOKEN || '',
 });
 
