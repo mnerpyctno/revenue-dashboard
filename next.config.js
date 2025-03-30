bd3233cda3bd5ae4b22466a00262ce6cd23b653b
mnerpyctno <artsemi.shyshkin@gmail.com>
2025-03-30 21:15:08 +0300

    Добавлены переменные окружения для Edge Config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    serverActions: true,
+  },
+  env: {
+    EDGE_CONFIG: process.env.EDGE_CONFIG,
+  },
 }
 
 module.exports = nextConfig 
